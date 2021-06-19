ef81c656a2fdc8ef3875e1700c3e1aeaa225d7f6
piyushdubey-pd <72221611+piyushdubey-pd@users.noreply.github.com>
2021-06-19 17:34:46 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const bodyparser = require("body-parser");
 const { render } = require("ejs");
 const mysql = require("mysql");
+var nodemailer =require('nodemailer');
+
 
 const app = express();
 
@@ -11,12 +13,18 @@ const connection = mysql.createConnection({
     password:"",
     database: "bmsar_db"
 });
-
 connection.connect(function(error) {
     if(error) throw error;
     else console.log("Connected to DB successfully")
 });
 
+
+var transporter=nodemailer.createTransport({
+    service:'gmail',
+    auth:{
+        user:'bmscearena@gmail.com',                                                                                  pass:'arenabmsce'
+    }
+});
 
 
 app.set("view engine" , "ejs");
@@ -96,12 +104,12 @@ app.get("/achivements" , function(req,res){
 
 
 app.get("/response" , function(req,res){
- F_name ="";
-L_name ="";
-Y_ear ="";
-U_sn ="";
-B_ranch ="";
-M_ail ="";
+    F_name ="";
+    L_name ="";
+    Y_ear ="";
+    U_sn ="";
+    B_ranch ="";
+    M_ail ="";
 
     res.render("response" ,  {title:"-Regis_Responsel",loginName:usn , Fname:"name" , Lname:"" , Year:Y_ear , Usn:U_sn , Branch : B_ranch , Mail:M_ail});
 })
@@ -130,9 +138,34 @@ app.post("/response" , function(req,res){
         res.render("registration",{title:"-event_Registration",loginName:usn , pptext:"***Please fill all the user credentials***" , fnamevalue:F_name ,lnamevalue :L_name,yearvalue :Y_ear , mailvalue :M_ail , branchvalue:B_ranch , usnvalue:U_sn});
     }
     else{
-    res.render("response", {title:"-Regis_Responsel",loginName:usn , Fname:F_name , Lname:L_name , Year:Y_ear , Usn:U_sn , Branch : B_ranch , Mail:M_ail});
+        if(U_sn.length==10){
+            connection.query("select * from user_reg where usn=?",[U_sn],function(error,results,fields){
+                console.log(results)
+                if(results.length>0)
+                res.render("registration",{title:"-event_Registration",loginName:usn , pptext:"***User has already registered***" , fnamevalue:F_name ,lnamevalue :L_name,yearvalue :Y_ear , mailvalue :M_ail , branchvalue:B_ranch , usnvalue:U_sn});
+                else
+                {
+                connection.query("insert into user_reg values(?, ?, ?, ?, ?, ?)",[F_name,L_name,Y_ear,U_sn,B_ranch,M_ail],function(error,results,fields){
+                res.render("response", {title:"-Regis_Responsel",loginName:usn , Fname:F_name , Lname:L_name , Year:Y_ear , Usn:U_sn , Branch : B_ranch , Mail:M_ail});
+           });
+        }
+        });
+            var mailOptions= {
+                from : 'bmscearena@gmail.com',
+                to: M_ail,
+                subject:'Successfull Registration',
+                text:'Congratulations on successfully registering in this event. \n All the best'
+            };
+            transporter.sendMail(mailOptions,function(error, info){
+                if(error)
+                console.log(error);
+                else
+                console.log('Email sent: '+info.response);
+            });
+        }
+        else
+        res.render("registration",{title:"-event_Registration",loginName:usn , pptext:"***Incorrec USN kidnly check***" , fnamevalue:F_name ,lnamevalue :L_name,yearvalue :Y_ear , mailvalue :M_ail , branchvalue:B_ranch , usnvalue:U_sn});       
 }
-    
 });
 
 
